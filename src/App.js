2a017f7ffa4210f1b285cf095f21091accf0bf17
Tom Irimescu <tom@irimescu.com>
2020-07-23 16:38:40 -0400

    Add subscription for on create note

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import {
   updateNote as UpdateNote,
   createNote as CreateNote,
   deleteNote as DeleteNote
-} from './graphql/mutations'
+} from './graphql/mutations';
+import { onCreateNote } from './graphql/subscriptions';
 
 const CLIENT_ID = uuid();
 
@@ -60,6 +61,17 @@ export default function App() {
 
   useEffect(() => {
     fetchNotes()
+    const subscription = API.graphql({
+      query: onCreateNote
+    })
+      .subscribe({
+        next: noteData => {
+          const note = noteData.value.data.onCreateNote
+          if (CLIENT_ID === note.clientId) return
+          dispatch({ type: 'ADD_NOTE', note })
+        }
+      })
+      return () => subscription.unsubscribe()
   }, [])
 
   async function createNote() {
